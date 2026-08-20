611a8f2d22a2e38b05f0394a6aab8d849f155a77
Zun Asha <zunashadev@gmail.com>
2026-08-20 15:03:01 +0700

    Prepare express backend for vercel

---
+++ api/index.js
@@ -0,0 +1,5 @@
+// INI DIGUNAKAN UNTUK PRODUCTION -> VERCEL -> SEBAGAI ENTRY POINT
+
+import app from "../app.js";
+
+export default app;
--- server.js
+++ server.js
@@ -1,27 +1,7 @@
-import express from "express";
-import "dotenv/config";
-import cors from "cors";
-import AuthRouter from "./routes/auth.route.js";
-import UserRouter from "./routes/user.route.js";
-import FollowRouter from "./routes/follow.route.js";
-import FeedRouter from "./routes/feed.route.js";
-import CommentRouter from "./routes/comment.route.js";
-import LikeRouter from "./routes/like.route.js";
-import BookmarkRouter from "./routes/bookmark.route.js";
+// INI DIGUNAKAN UNTUK DEVELOPMENT -> LOCAL
+import app from "./app.js";
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/auth", AuthRouter);
-app.use("/api/user", UserRouter);
-app.use("/api/follow", FollowRouter);
-app.use("/api/feed", FeedRouter);
-app.use("/api/comment", CommentRouter);
-app.use("/api/like", LikeRouter);
-app.use("/api/bookmark", BookmarkRouter);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
